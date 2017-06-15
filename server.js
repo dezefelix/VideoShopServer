79d288596f3502589d734afc975242b977aca163
jesse <jwit11@avans.nl>
2017-06-15 12:48:24 +0200

    Init

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jesse on 15-06-2017.
+ */
+var config = require('./config.json');
+
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.set('PORT', config.webPort);
+var port = app.get('PORT');
+
+app.all('*', function(request, response, next) {
+    console.log(request.method + " " + request.url);
+    next();
+});
+
+app.use('/api/v1', require('./routes/routes_api_v1'));
+
+app.listen(port, function() {
+    console.log('Server is running on port 8080');
+});
+
+module.exports = app;
