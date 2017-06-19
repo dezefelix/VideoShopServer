9496fe8a442ecaf5848b63995b576d5e405ea279
jesse <jwit11@avans.nl>
2017-06-19 13:06:14 +0200

    Little fix

--- routes/routes_api_v1.js
+++ routes/routes_api_v1.js
@@ -167,7 +167,7 @@ router.get('/getcopies/:filmid', function (req, res) {
             if (error) {
                 res.status(400).json({"Retrieve films": "failed"});
             } else {
-                res.status(200).json(rows);
+                res.status(200).json({"copies":rows});
             }
             connection.release();
         });
